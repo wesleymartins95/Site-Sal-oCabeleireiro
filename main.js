00de1ef5b90dfec8822e65e15edb8a4040f32d5f
wesley martins <wesleyat@outlook.com>
2022-04-01 12:08:55 -0300

    Penultima versão

--- main.js
+++ main.js
@@ -56,7 +56,7 @@ const swiper = new Swiper('.swiper-container', {
   keyboard: true,
   breakpoints: {
     767: {
-      slidesPerView: 1,
+      slidesPerView: 2 ,
       setWrapperSize: true
     }
   }
